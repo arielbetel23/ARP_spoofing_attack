374a4adc347dd7a5db41bee475cddc45bb02b5d5
Ariel Betel <arielbetel7@gmail.com>
2026-07-30 18:41:16 +0300

    fixed the import and added a function the retrives teh MAC adress from the IP adress

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import scapy
+from scapy.all import ARP, Ether, srp
 
 IP_adress = "192.168.1.187"
+
+def get_MAC_adress():
+    global IP_adress
+    request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=IP_adress)
+    answered, unanswered = srp(request, timeout=2, verbose=False)
+    MAC_adress = answered[0][1].hwsrc
+    return MAC_adress
 
